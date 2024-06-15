9f37df6fbc65c9f5db6f10300cb3c62050e20cd0
Al Noman <alnomaan30@gmail.com>
2024-06-15 12:29:47 +0600

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Product from './Product';
+
+
+
+ function Products({products}) {
+  return (
+    <div className='products'>
+        
+        {products.map((product) => 
+           <Product key={product.id} product={product}/>
+        
+        )}
+        
+      
+    </div>
+  )
+}
+export default Products;
